b6abf3476a2ef045b4138bb778e7e98ebc2cf65c
xhiti <mexhitkurti10@gmail.com>
2021-12-14 16:13:38 +0100

    xhiti

--- DATA_Assignment_1.py
+++ DATA_Assignment_1.py
@@ -1,14 +1,14 @@
-import pandas as pd
-import numpy as np
+import pandas
+import numpy
 import statsmodels.formula.api as stats_formula
 import statsmodels.stats.multicomp as multicomp
 import matplotlib.pyplot as pyplot
 
 # read from dataset
-data = pd.read_csv('dataset/marscrater_pds.csv', low_memory=False)
+data = pandas.read_csv('dataset/marscrater_pds.csv', low_memory=False)
 
 # bug fix for display formats to avoid run time errors
-pd.set_option('display.float_format', lambda x:'%f'%x)
+pandas.set_option('display.float_format', lambda x:'%f'%x)
 
 # setting variables to numeric
 data['LATITUDE_CIRCLE_IMAGE'] = pandas.to_numeric(data['LATITUDE_CIRCLE_IMAGE'], errors='coerce')
@@ -45,4 +45,57 @@ print("# ==================== #")
 print("# Summary for Model 1: #")
 print("# ==================== #")
 print()
+print(model_1.summary())
+print()
 
+
+working_subset_3 = working_subset_2[['DEPTH_RIMFLOOR_TOPOG', 'MARS_REGION']].dropna()
+print()
+average_1 = working_subset_3.groupby('MARS_REGION').mean()
+print("# ================================================== #")
+print("# Mesatarja e thellesive te kratereve sipas grupeve: #")
+print("# ================================================== #")
+print(average_1)
+print()
+
+print()
+standart_deviation_1 = working_subset_3.groupby('MARS_REGION').std()
+print("# ========================================================== #")
+print("# Devijimi standart i thellesive te kratereve sipas grupeve: #")
+print("# ========================================================== #")
+print(standart_deviation_1)
+print()
+
+# Post Hoc Test
+multi_comparison_1 = multicomp.MultiComparison(
+    working_subset_3['DEPTH_RIMFLOOR_TOPOG'],
+    working_subset_3['MARS_REGION']
+)
+result_1 = multi_comparison_1.tukeyhsd()
+print("# ================ #")
+print("# Tukeyhsd Result: #")
+print("# ================ #")
+print(result_1.summary())
+print()
+
+# groups means visualisation
+box_props = dict(linewidth=1.2)
+mean_point_props = dict(marker='D', markeredgecolor='black', markerfacecolor='firebrick')
+figure, ax = pyplot.subplots(figsize=(10, 12))
+figure = working_subset_3.boxplot(
+    'DEPTH_RIMFLOOR_TOPOG',
+    'MARS_REGION',
+    boxprops=box_props,
+    meanprops=mean_point_props,
+    meanline=False,
+    showmeans=True,
+    ax=ax,
+    grid=False
+)
+
+pyplot.xlabel('MARS_REGION')
+pyplot.ylabel('DEPTH_RIMFLOOR_TOPOG')
+pyplot.title('Crater Depth BoxhPlot group by Mars Region', y=1.02)
+pyplot.tick_params(axis='both')
+pyplot.show()
+ax.figure.savefig('output_DAT_assignment_1,png')
